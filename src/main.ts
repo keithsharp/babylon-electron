74a7550e9f2c4d6e6808e9c5f79504871b5f518c
Keith Sharp <kms@passback.co.uk>
2021-03-27 09:56:53 +0000

    Working Electron application rendering a 3D sphere.

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import * as url from "url";
+
+let mainWindow: Electron.BrowserWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: {
+        contextIsolation: false,
+        nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, "../index.html"),
+      protocol: "file:",
+      slashes: true,
+  }));
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On OS X it"s common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
